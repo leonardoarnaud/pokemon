a1f8e93c9a6a1e006e3ef558642b2b3d7b3d808f
Arnaud <leonardo.arnaud@stormsec.com.br>
2020-06-06 10:37:12 -0300

    Implementação da searchview com suggestions

--- app/src/main/java/br/eti/arnaud/pokemon/SearchActivity.kt
+++ app/src/main/java/br/eti/arnaud/pokemon/SearchActivity.kt
@@ -1,17 +1,28 @@
 package br.eti.arnaud.pokemon
 
+import android.content.Context
+import android.database.Cursor
+import android.database.MatrixCursor
 import android.view.Menu
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.cursoradapter.widget.CursorAdapter
+import com.bumptech.glide.Glide
 import java.util.*
+
 
 abstract class SearchActivity : AppCompatActivity() {
 
+    private lateinit var searchView: SearchView
     private var searchTimer: Timer = Timer()
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_activity_main, menu)
-        (menu?.findItem(R.id.action_search)?.actionView as SearchView).apply {
+        searchView = (menu?.findItem(R.id.action_search)?.actionView as SearchView)
+        searchView.apply {
             maxWidth = Integer.MAX_VALUE
             setOnQueryTextListener({
                 onSearchTextSubmit(it)
@@ -27,6 +38,32 @@ abstract class SearchActivity : AppCompatActivity() {
         }
         return super.onCreateOptionsMenu(menu)
     }
+
+    var searchSuggestions: List<Pair<String, String>>? = null
+        set(value) {
+            val cursor = MatrixCursor(arrayOf("_id", "image", "title"))
+            value?.mapIndexed { i, v ->
+                cursor.newRow()
+                    .add("_id", i)
+                    .add("image", v.first)
+                    .add("title", v.second)
+            }
+            searchView.suggestionsAdapter = object : CursorAdapter(this, cursor, false) {
+                override fun newView(context: Context?, cursor: Cursor?, parent: ViewGroup?): View {
+                    return layoutInflater.inflate(R.layout.item_search_suggestion, parent, false)
+                }
+
+                override fun bindView(view: View?, context: Context?, cursor: Cursor?) {
+                    view?.let {
+                        Glide.with(it)
+                            .load(cursor?.getString(1))
+                            .into(it.findViewById(R.id.itemImageView))
+                        it.findViewById<TextView>(R.id.titleTextView).text = cursor?.getString(2)
+                    }
+                }
+            }
+            field = value
+        }
 
     abstract fun onSearchTextSubmit(text: String)
     abstract fun onSearchTextChange(text: String)
---
+++ app/src/main/java/br/eti/arnaud/pokemon/SearchCursorAdapter.kt
@@ -0,0 +1,25 @@
+package br.eti.arnaud.pokemon
+
+import android.content.Context
+import android.database.Cursor
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CursorAdapter
+
+class SearchCursorAdapter(
+    private val layoutInflater: LayoutInflater,
+    context: Context?,
+    c: Cursor?,
+    autoRequery: Boolean
+) :
+    CursorAdapter(context, c, autoRequery) {
+
+    override fun newView(context: Context?, cursor: Cursor?, parent: ViewGroup?): View {
+        return layoutInflater.inflate(R.layout.item_search_suggestion, parent, false)
+    }
+
+    override fun bindView(view: View?, context: Context?, cursor: Cursor?) {
+        TODO("Not yet implemented")
+    }
+}
